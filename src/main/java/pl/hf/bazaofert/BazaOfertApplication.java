ef86257c2a702460c5685a695da7ab6aa7103e9c
Jasiek Wolnicki <wolnickij@gmail.com>
2019-07-06 09:37:54 +0200

    Initial commit. Empty application in spring

---
+++ src/main/java/pl/hf/bazaofert/BazaOfertApplication.java
@@ -0,0 +1,13 @@
+package pl.hf.bazaofert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaOfertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BazaOfertApplication.class, args);
+    }
+
+}
